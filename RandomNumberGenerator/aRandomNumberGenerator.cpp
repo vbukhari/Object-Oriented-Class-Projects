7fa99b010fd9190bf366057c390bb5b625c9672e
Vasim Bukhari <vbukhari86@gmail.com>
2012-10-07 04:28:43 -0400

    Object-Oriented Project 1 using MS Visual Studio

---
+++ RandomNumberGenerator/aRandomNumberGenerator.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include "aRandomNumberGenerator.h"
+
+using namespace std;
+
+int main() {
+
+cout << "hello" << endl;
+
+aRandomNumberGenerator r;
+Tracker t;
+
+r.setLow(1);
+r.setHigh(9);
+
+for (int i = 0; i < 20; i++) {
+
+int randomNumber = r.generate();
+
+t.trackInt(randomNumber);
+
+cout << randomNumber << " ";
+
+}
+
+cout << endl;
+return 0;
+}
