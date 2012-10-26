a030f16c7e73e02f11fa7dee297ea6ea0c2427cf
Luqmaan <intern@PC-2.local>
2012-10-26 12:17:59 -0400

    The main now accepts user input

--- Project 2/main.cpp
+++ Project 2/main.cpp
@@ -11,28 +11,38 @@ using namespace std;
 
 int main() {
 
-	aHistogram h;
-	h.setRange (1, 6);
-
-	aDie d;
-	for (int i = 0; i < 6000; i++) {
-		h.update( d.throwDie() );
-	}
-	h.printHistogram();
-
-	cout << endl << "HELLO COIN!" << endl;
-
-	h.clear();
-	h.setRange(1,2);
+	int timesToThrowDie, timesToFlipCoin;
 
 	aCoin c;
-	for (int i = 0; i < 6000; i++) {
+	aDie d;
+	aHistogram h;
+
+	cout << "Enter the number of times to flip the coin: ";
+	cin >> timesToFlipCoin;
+	cout << "Enter the number of times to throw the die: ";
+	cin >> timesToThrowDie;
+
+	cout << endl << "The coin yielded: " << endl;
+	h.setRange(1,2);
+	for (int i = 0; i < timesToFlipCoin; i++) {
 		h.update( c.flipCoin() );
 	}
 	h.printHistogram();
 
+	h.clear();
+
+	cout << endl << "The die yielded: " << endl;
+	h.setRange (1, 6);
+	for (int i = 0; i < timesToThrowDie; i++) {
+		h.update( d.throwDie() );
+	}
+	h.printHistogram();
+
+
 	// http://stackoverflow.com/questions/2529617/how-to-stop-c-console-application-from-exiting-immediately
-	getchar();
+	getchar();getchar();getchar();getchar();getchar();getchar();
+
 	return 0;
+
 }
 
